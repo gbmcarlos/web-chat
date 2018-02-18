6faabbefb1f19c6421a46da7f1ff72338ed4cd6c
Carlos Garcia-Berro Molero <gbmcarlos@gmail.com>
2018-02-18 19:17:29 +0400

    db connection and middleweares registering added to app.php

--- www/web/app.php
+++ www/web/app.php
@@ -14,9 +14,22 @@ $app = new Silex\Application();
 // Set debug from env var
 $app['debug'] = getenv('APP_DEBUG');
 
+// Set up db connections and set schema
+$db = new DB\SQLiteDatabase();
+$db->connect(getenv('SQLITEDB_FILE'));
+$schema = require_once(__DIR__ . '/../db/schema.php');
+$db->create($schema);
+
+// Register db in the service container
+$app['DB'] = function () use ($db) {
+    return $db->getPDO();
+};
+
 // Register app service, similar to registering the bundles in the app kernel in symfony
 \App\config\Services::registerServices($app);
 
 \App\config\Routing::registerRoutes($app);
+
+\App\config\Middleware::registerMiddlewares($app);
 
 $app->run();
